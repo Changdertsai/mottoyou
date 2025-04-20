8d3132a40ee02d8dcdf0baecfb65114aa055b325
Changdertsai <carlarkzhang@gmail.com>
2025-04-20 19:25:27 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // 项目根目录
+  publicDir: 'public', // 静态资源目录
   plugins: [react()],
-});
+  build: {
+    outDir: 'dist', // 构建输出目录
+  },
+})
