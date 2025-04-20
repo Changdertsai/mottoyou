9bb908c5d95beeaf6bdcc0d953a44892506d8ba7
Changdertsai <carlarkzhang@gmail.com>
2025-04-20 19:00:37 +0700

    Create app.tsx

---
+++ scr/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px", fontFamily: "sans-serif" }}>
+      <h1>Welcome to <strong>MottoYou</strong></h1>
+      <p>"You are not alone."</p>
+    </div>
+  );
+}
+
+export default App;
